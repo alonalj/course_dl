2226cb818d69aae68bff7bf67e79fa6346cae484
Gil Shomron <gil.shomron@gmail.com>
2019-01-12 22:10:03 +0200

    HW4: progress

--- hw4/main.py
+++ hw4/main.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pdb
 from sklearn.model_selection import train_test_split
 from keras.datasets import imdb
 from keras.models import Sequential
@@ -13,7 +12,7 @@ from keras.models import Model
 # load the dataset but only keep the top words, zero the rest. Introduce special tokens.
 VOCABULARY_SIZE = 20000
 MAX_SAMPLES = 40000
-MAX_LEN = 256
+MAX_LEN = 100
 
 (sentences, sentiment), _ = imdb.load_data(num_words=VOCABULARY_SIZE)
 
@@ -25,82 +24,152 @@ word_to_id["<OOV>"] = 2
 word_to_id["<EOS>"] = 3
 id_to_word = {v: k for k, v in word_to_id.items()}
 
-X_train = sequence.pad_sequences(sentences, maxlen=MAX_LEN, truncating='post')
-y_train = np.roll(X_train, -1, axis=-1)
-y_train[:, -1] = word_to_id["<EOS>"]
-#y_train = y_train[:, :, np.newaxis]
 
-X_train_ext = []
-y_train_ext = []
-
-# For each sentence
-for i, x in enumerate(X_train):
-    # For each word in the sentence
-    for j in range(2, len(x)):
-        if len(X_train_ext) >= MAX_SAMPLES:
-            break
-
-        X_train_ext.append(sequence.pad_sequences([x[:j]], maxlen=MAX_LEN, truncating='post', padding='post')[0])
-        y_train_ext.append(to_categorical([x[j]], num_classes=VOCABULARY_SIZE)[0])
-
-X_train = np.array(X_train_ext)
-y_train = np.array(y_train_ext)
-#X_train = np.array(X_train)
-#y_train = np.array(y_train)
-#y_train = np.array([to_categorical(y, num_classes=VOCABULARY_SIZE) for y in y_train])
-#y_train = y_train[:, :, np.newaxis]
-
-X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.1)
-
-print(X_train.shape)
-print(y_train.shape)
-
-LSTM_SIZE = 256
-
-input = Input(shape=(MAX_LEN, ))
-se1 = Embedding(VOCABULARY_SIZE, 100, input_length=MAX_LEN, mask_zero=True)(input) #
-se2 = Dropout(0.3)(se1)
-
-# Stack 1
-lstm1 = LSTM(LSTM_SIZE, return_sequences=True)(se2)
-bn1 = BatchNormalization()(lstm1)
-do1 = Dropout(0.3)(bn1)
-
-# Stack 2
-lstm2 = LSTM(LSTM_SIZE, return_sequences=True)(do1)
-bn2 = BatchNormalization()(lstm2)
-do2 = Dropout(0.3)(bn2)
-
-# Stack 3
-lstm3 = LSTM(LSTM_SIZE)(do2)
-bn3 = BatchNormalization()(lstm3)
-do3 = Dropout(0.3)(bn3)
-
-output = Dense(VOCABULARY_SIZE, activation='softmax')(do3)
-
-model = Model(inputs=input, outputs=output)
+def sample(preds, temperature=1.0):
+    """Helper function to sample an index from a probability array"""
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
 
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam', metrics=['accuracy'])
-print(model.summary())
-model.fit(X_train, y_train,
-          validation_data=(X_test, y_test),
-          epochs=5, batch_size=128)
+def word_level_time_dist():
+    LSTM_SIZE = 256
+
+    X_train = sequence.pad_sequences(sentences, maxlen=MAX_LEN, truncating='post')
+    y_train = np.roll(X_train, -1, axis=-1)
+    y_train = y_train[:, :, np.newaxis]
+
+    X_train, X_test, y_train, y_test = train_test_split(np.array(X_train), np.array(y_train), test_size=0.1)
+
+    input = Input(shape=(MAX_LEN, ))
+    se1 = Embedding(VOCABULARY_SIZE, 200, input_length=MAX_LEN)(input) #, mask_zero=True
+    se2 = Dropout(0.3)(se1)
+
+    # Stack 1
+    lstm1 = CuDNNLSTM(LSTM_SIZE, return_sequences=True)(se2)
+    bn1 = BatchNormalization()(lstm1)
+    do1 = Dropout(0.3)(bn1)
+
+    # Stack 2
+    lstm2 = CuDNNLSTM(LSTM_SIZE, return_sequences=True)(do1)
+    bn2 = BatchNormalization()(lstm2)
+    do2 = Dropout(0.3)(bn2)
+
+    # Stack 3
+    lstm3 = CuDNNLSTM(LSTM_SIZE, return_sequences=True)(do2)
+    bn3 = BatchNormalization()(lstm3)
+    do3 = Dropout(0.3)(bn3)
+
+    # Stack 4
+    lstm4 = CuDNNLSTM(LSTM_SIZE, return_sequences=True)(do3)
+    bn4 = BatchNormalization()(lstm4)
+    do4 = Dropout(0.3)(bn4)
+
+    fc1 = TimeDistributed(Dense(512, activation='relu'))(do4)
+    bn5 = BatchNormalization()(fc1)
+    do5 = Dropout(0.3)(bn5)
+    output = TimeDistributed(Dense(VOCABULARY_SIZE, activation='softmax', input_shape=(MAX_LEN, LSTM_SIZE)))(do5)
+
+    model = Model(inputs=input, outputs=output)
+
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='adam')
+    print(model.summary())
+    model.fit(X_train, y_train,
+              validation_data=(X_test, y_test),
+              epochs=20, batch_size=128)
+
+    seed = []
+    seed.append(word_to_id['<START>'])
+    seed.append(word_to_id['the'])
+    seed.append(word_to_id['movie'])
+    seed.append(word_to_id['was'])
+
+    for i in range(3, MAX_LEN):
+        model.reset_states()
+        seed_padded = sequence.pad_sequences([seed], maxlen=MAX_LEN, truncating='post', padding='post')
+        y = model.predict(seed_padded)[0][i]
+        next_word_id = sample(y, temperature=0.7)
+        seed.append(next_word_id)
+
+    for i in range(MAX_LEN):
+        print("{} ".format(id_to_word[seed[i]]))
 
 
-seed = []
-seed.append(word_to_id['<START>'])
-seed.append(word_to_id['the'])
-seed.append(word_to_id['movie'])
-seed.append(word_to_id['was'])
 
+def word_level():
+    X_train = sequence.pad_sequences(sentences, maxlen=MAX_LEN, truncating='post')
+    y_train = np.roll(X_train, -1, axis=-1)
+    y_train[:, -1] = word_to_id["<EOS>"]
+    # y_train = y_train[:, :, np.newaxis]
 
-for i in range(MAX_LEN-4):
-    seed_padded = sequence.pad_sequences([seed], maxlen=MAX_LEN, truncating='post', padding='post')
-    result = model.predict(seed_padded)
-    id = np.argmax(result)
-    seed.append(id)
+    X_train_ext = []
+    y_train_ext = []
 
-for i in range(MAX_LEN):
-    print("{} ", id_to_word[seed[i]])
+    # For each sentence
+    for i, x in enumerate(X_train):
+        # For each word in the sentence
+        for j in range(2, len(x)):
+            if len(X_train_ext) >= MAX_SAMPLES:
+                break
+
+            X_train_ext.append(sequence.pad_sequences([x[:j]], maxlen=MAX_LEN, truncating='post', padding='post')[0])
+            y_train_ext.append(to_categorical([x[j]], num_classes=VOCABULARY_SIZE)[0])
+
+    X_train = np.array(X_train_ext)
+    y_train = np.array(y_train_ext)
+
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.1)
+
+    print(X_train.shape)
+    print(y_train.shape)
+
+    LSTM_SIZE = 256
+
+    input = Input(shape=(MAX_LEN,))
+    se1 = Embedding(VOCABULARY_SIZE, 100, input_length=MAX_LEN, mask_zero=True)(input)  #
+    se2 = Dropout(0.3)(se1)
+
+    # Stack 1
+    lstm1 = LSTM(LSTM_SIZE, return_sequences=True)(se2)
+    bn1 = BatchNormalization()(lstm1)
+    do1 = Dropout(0.3)(bn1)
+
+    # Stack 2
+    lstm2 = LSTM(LSTM_SIZE, return_sequences=True)(do1)
+    bn2 = BatchNormalization()(lstm2)
+    do2 = Dropout(0.3)(bn2)
+
+    # Stack 3
+    lstm3 = LSTM(LSTM_SIZE)(do2)
+    bn3 = BatchNormalization()(lstm3)
+    do3 = Dropout(0.3)(bn3)
+
+    output = Dense(VOCABULARY_SIZE, activation='softmax')(do3)
+
+    model = Model(inputs=input, outputs=output)
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
+    model.fit(X_train, y_train,
+              validation_data=(X_test, y_test),
+              epochs=5, batch_size=128)
+
+    seed = []
+    seed.append(word_to_id['<START>'])
+    seed.append(word_to_id['the'])
+    seed.append(word_to_id['movie'])
+    seed.append(word_to_id['was'])
+
+    for i in range(MAX_LEN - 4):
+        seed_padded = sequence.pad_sequences([seed], maxlen=MAX_LEN, truncating='post', padding='post')
+        result = model.predict(seed_padded)
+        id = np.argmax(result)
+        seed.append(id)
+
+    for i in range(MAX_LEN):
+        print("{} ".format(id_to_word[seed[i]]))
