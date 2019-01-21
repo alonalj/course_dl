be1397c9ab9ea64c92b7509417ff3e373e3d3952
alonalj <alonajurgenson@gmail.com>
2019-01-21 20:12:43 +0200

    penalty post softmax

--- project/model.py
+++ project/model.py
@@ -234,7 +234,7 @@ def run(c):
     batch_size = 128
     # adam = optimizers.Adam()
     if c.n_tiles_per_sample > 6:
-        batch_size = 100
+        batch_size = 80
     if c.n_tiles_per_sample > 20:
         batch_size = 100
         # adam = optimizers.Adam(0.0001)
