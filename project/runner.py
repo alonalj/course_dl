144c21c07ad93680188b62d849c870eea90a5841
alonalj <alonajurgenson@gmail.com>
2019-01-21 20:05:05 +0200

    penalty post softmax

--- project/runner.py
+++ project/runner.py
@@ -35,8 +35,8 @@ if sample:
 else:
     for i in range(3):  # for majority vote
         mID = str(i)+"_"+str(time.time())
-        for is_images in [True, False]: #[True]:  #
-            for tiles_per_dim in [2, 4, 5]: # [4]: #
+        for is_images in [True]: #[True]:  #
+            for tiles_per_dim in [4]: # [4]: #
                 if tiles_per_dim == 2:
                     max_size = base_max_size
                 elif tiles_per_dim == 4:
