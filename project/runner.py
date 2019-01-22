d0352145415dda368791c9c9e576a5d2b6c3be85
alonalj <alonajurgenson@gmail.com>
2019-01-22 23:44:39 +0200

    train loss save name fixed

--- project/runner.py
+++ project/runner.py
@@ -35,8 +35,8 @@ if sample:
 else:
     for i in range(3):  # for majority vote
         mID = str(i)+"_"+str(time.time())
-        for is_images in [False]: #[True]:  #
-            for tiles_per_dim in [5]: # [4]: #
+        for is_images in [True]: #[True]:  #
+            for tiles_per_dim in [4]: # [4]: #
                 if tiles_per_dim == 2:
                     max_size = base_max_size
                 elif tiles_per_dim == 4:
