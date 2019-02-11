da0ac216b831f8d11ce01bf5dec8a6cdbca9376c
alonalj <alonajurgenson@gmail.com>
2019-02-11 17:23:22 +0200

    preprocessor for ood

--- project/preprocessor.py
+++ project/preprocessor.py
@@ -672,6 +672,8 @@ def create_ood_non_ood_pairs(isImg):
                 OUTPUT_DIR = OUTPUT_DIR_TEST
 
             files_for_t = glob.glob(IM_DIR + '*t_{}*'.format(tiles_per_dim))
+            np.random.shuffle(files_for_t)
+            files_for_t[:300]
             files_for_t = [f.split('/')[-1] for f in files_for_t]
 
             if isImg:
