8541bcf5ec1435f3b152f3f8a853ecb102434db3
alonalj <alonajurgenson@gmail.com>
2019-02-12 22:19:11 +0200

    preprocessor for ood

--- project/runner_cols_rows.py
+++ project/runner_cols_rows.py
@@ -4,12 +4,13 @@ from resnet_rows_cols_folder_based import *
 for i in range(1):  # for majority vote
     # mID = str(i) + "_" + str(time.time())
     for is_images in [True]:
-        for tiles_per_dim in [5, 4]:  # [4]: #
-            if tiles_per_dim == 5:
-                rows_or_cols_list = ["rows"]
-            else:
-                rows_or_cols_list = ["cols"]
-            for rows_or_cols in rows_or_cols_list:
+        for tiles_per_dim in [2,4,5]:  # [4]: #
+            # for rows_or_cols in :
+            # if tiles_per_dim == 5:
+            #     rows_or_cols_list = ["rows"]
+            # else:
+            #     rows_or_cols_list = ["cols"]
+            for rows_or_cols in ["rows", "cols"]:
                 c = Conf(tiles_per_dim=tiles_per_dim, max_size=112, is_images=is_images)
                 print(c.n_tiles_per_sample)
                 shred_for_rows_cols(is_images, tiles_per_dim, c)
