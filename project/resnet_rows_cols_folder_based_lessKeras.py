690c74d81ece350a48d2ca093b8d3b340ec4d1f2
alonalj <alonajurgenson@gmail.com>
2019-02-14 17:47:25 +0200

    resnet keras

--- project/resnet_rows_cols_folder_based_lessKeras.py
+++ project/resnet_rows_cols_folder_based_lessKeras.py
@@ -8,8 +8,17 @@ from keras.regularizers import Regularizer
 from keras import backend as K
 import numpy as np
 from keras_preprocessing.image import ImageDataGenerator
-from preprocessor import preprocess_image
+from preprocessor import *
 from conf import Conf
+
+def get_train_steps(c, batch_size):
+    path = "dataset_rows_cols_{}_isImg_{}/".format(c.tiles_per_dim, c.is_images)
+    folders = os.listdir(path)
+    relevant_files = load_obj('files_{}_img_{}'.format("train", c.is_images))
+    train_len = len(relevant_files) * c.tiles_per_dim ** 2
+    relevant_files = [f for f in folders if f.split('.')[0][:-3] + '.jpg' in relevant_files]  # tTODO: for images too
+    # n_per_class = batch_size // c.tiles_per_dim
+    return train_len // batch_size
 
 def data_generator(data_type, tiles_per_dim, data_split_dict, batch_size, c, rows_or_cols):
     import random
@@ -17,43 +26,41 @@ def data_generator(data_type, tiles_per_dim, data_split_dict, batch_size, c, row
     import os
     from keras.utils import to_categorical
 
-    path = "{}_{}_isImg_{}".format(rows_or_cols, tiles_per_dim, c.is_images)
-    if data_type == "val":
-        path = path+'_val'
-    if data_type == "test":
-        path = path + '_test'
+    path = "dataset_rows_cols_{}_isImg_{}/".format(c.tiles_per_dim, c.is_images)
+
+    relevant_files = load_obj('files_{}_img_{}'.format(data_type, c.is_images))
+
     folders = os.listdir(path)
-    train_len = len(glob.glob(path+'/0'+'/*')) * c.tiles_per_dim
+    train_len = len(relevant_files) * c.tiles_per_dim**2
+    relevant_files = [f for f in folders if f.split('.')[0][:-3]+'.jpg' in relevant_files] #tTODO: for images too
+    # n_per_class = batch_size // c.tiles_per_dim
     while True:
-        for i in range(train_len // batch_size):
-            X_batch = []
-            y_batch = []
-            for class_folder in folders:
-                label = class_folder
-                folders_in_class = glob.glob(path+'/'+class_folder+'/*')
-                np.random.shuffle(folders_in_class)  # random shuffle files in folders too  #TODO: evaluate uses sorted files... is this necessary?
-                for f in folders_in_class[:batch_size//c.tiles_per_dim]: # because of random shuffle above, will be different between yields
-                    # files_in_folder = glob.glob(folder+'/*')
-                    # combined_images = []
-                    im = cv2.imread(f)
-                    im = preprocess_image(im, c)
-                    X_batch.append(im)
-                    y_batch.append(to_categorical(label, num_classes=c.tiles_per_dim))
-                        # combined_images.append(im / 255.)
+        # for i in range(get_train_steps(c, batch_size)):
+        labels_per_class = []
+        X_batch = []
+        y_batch = []
+        # files = relevant_files[:batch_size]
+        random.shuffle(relevant_files)
+        for f in relevant_files[:batch_size]:
 
-            # if len(y_batch) == batch_size:
-            zipped = list(zip(X_batch, y_batch))
-            random.shuffle(zipped)
-            X_batch, y_batch = zip(*zipped)
+            if 'DS' in f:
+                continue
+            # print(f)
+            label = get_row_col_label(f[0].split('.')[0][-2:],c.tiles_per_dim,rows_or_cols) #TODO verify for imgs
 
-            # if np.array(X_batch).shape[1:] != (2, c.max_size, c.max_size, 2):
-            #     print(folder)
-            #     print(np.array(X_batch).shape)
-            # print(list(np.array(y_batch).reshape(1, batch_size, 2)))
-            combined_images, labels = np.array(X_batch), np.array(y_batch)
-            # print(labels)
-            yield combined_images, labels
+            im = cv2.imread(path+f)
+            im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+            im = preprocess_image(im, c)
+            if im.shape != (c.max_size, c.max_size, 1):
+                continue
+            # if to_categorical(label, num_classes=c.tiles_per_dim).shape != c.tiles_per_dim:
+            #     continue
+            X_batch.append(im)
+            y_batch.append(to_categorical(label, num_classes=c.tiles_per_dim))
 
+        combined_images, labels = np.array(X_batch), np.array(y_batch)
+        # print(labels)
+        yield combined_images, labels
 
 
 import cv2
@@ -73,12 +80,12 @@ from conf import Conf
 def res_2_layer_block_img_vs_doc(x_in, dim, downsample=False, weight_decay=0.0001):
     x = Conv2D(dim, kernel_size=(3, 3), padding='same', strides=(2, 2) if downsample else (1, 1),
                kernel_regularizer=regularizers.l2(weight_decay))(x_in)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     x = Conv2D(dim, kernel_size=(3, 3), padding='same', strides=(1, 1),
                kernel_regularizer=regularizers.l2(weight_decay))(x)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
 
     if downsample:
         x_in = Conv2D(dim, kernel_size=(1, 1), padding='same', strides=(2, 2),
@@ -99,17 +106,17 @@ def res_tower_2_layer_img_vs_doc(x, dim, num_layers, downsample_first=True, weig
 def res_3_layer_block_img_vs_doc(x_in, dim_reduce, dim_out, downsample=False, adjust_skip_dim=False, weight_decay=0.0001):
     x = Conv2D(dim_reduce, kernel_size=(1, 1), padding='same', strides=(2, 2) if downsample else (1, 1),
                kernel_regularizer=regularizers.l2(weight_decay))(x_in)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     x = Conv2D(dim_reduce, kernel_size=(3, 3), padding='same', strides=(1, 1),
                kernel_regularizer=regularizers.l2(weight_decay))(x)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     x = Conv2D(dim_out, kernel_size=(1, 1), padding='same', strides=(1, 1),
                kernel_regularizer=regularizers.l2(weight_decay))(x)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
 
     if downsample or adjust_skip_dim:
         x_in = Conv2D(dim_out, kernel_size=(1, 1), padding='same', strides=(2, 2) if downsample else (1, 1),
@@ -132,7 +139,7 @@ def build_resnet_rows_col(TILES_PER_DIM, SHAPE, weight_decay=1e-3):
     x_in = Input(shape=(SHAPE, SHAPE, 1))
     x = Conv2D(64, kernel_size=(3, 3), padding='same', strides=(1, 1),
                kernel_regularizer=regularizers.l2(weight_decay))(x_in)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     x = res_tower_2_layer_img_vs_doc(x, 64, 2, False, weight_decay=weight_decay)
@@ -180,27 +187,38 @@ def run(c, rows_or_cols):
     batch_size = 130
     # TODO: check withoutval in row below
 
-    steps_per_epoch = len(os.listdir("{}_{}_isImg_{}/0/".format(rows_or_cols, tiles_per_dim, c.is_images)))*tiles_per_dim // batch_size
-    maxepoches = 100
+    steps_per_epoch = get_train_steps(c,batch_size)#len(os.listdir("{}_{}_isImg_{}/0/".format(rows_or_cols, tiles_per_dim, c.is_images)))*tiles_per_dim // batch_size
+    maxepoches = 10
     learning_rate = 0.0001
     # reduce_lr = keras.callbacks.LearningRateScheduler(lr_scheduler)
-    reduce_lr = keras.callbacks.ReduceLROnPlateau(patience=50, min_lr=0.00001)
+    reduce_lr = keras.callbacks.ReduceLROnPlateau(patience=5, min_lr=0.00001,verbose=1)
 
     sgd = optimizers.SGD(lr=learning_rate, momentum=0.9, nesterov=True)
 
-    resnet_rows_cols.compile(
-        loss='categorical_crossentropy',
-        optimizer='adam',
-        metrics=['accuracy']
-    )
-    resnet_rows_cols.summary()
+
+
 
     datagen_img_vs_doc_train = data_generator('train', c.tiles_per_dim, '', batch_size, c, rows_or_cols)#ImageDataGenerator()#preprocessing_function=to_grayscale)
 
     datagen_img_vs_doc_val = data_generator('val', c.tiles_per_dim, '', batch_size, c, rows_or_cols)#ImageDataGenerator()#preprocessing_function=to_grayscale)
 
+    from keras.applications.resnet50 import ResNet50
+    resnet_rows_cols = ResNet50(include_top=False, weights=None, input_tensor=None, input_shape=(c.max_size,c.max_size,1),
+                                       pooling=None, classes=c.tiles_per_dim)
+    # Add final layers
+    x = resnet_rows_cols.output
+    x = Flatten()(x)
+    predictions = Dense(c.tiles_per_dim, activation='softmax', name='fc1000')(x)
 
-        # featurewise_center=False,  # set input mean to 0 over the dataset
+    # This is the model we will train
+    model = Model(input=resnet_rows_cols.input, output=predictions)
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
+    model.summary()
+    # featurewise_center=False,  # set input mean to 0 over the dataset
         # samplewise_center=False,  # set each sample mean to 0
         # featurewise_std_normalization=False,  # divide inputs by std of the dataset
         # samplewise_std_normalization=False,  # divide each input by its std
@@ -212,20 +230,34 @@ def run(c, rows_or_cols):
         # vertical_flip=False)  # randomly flip images
 
     # datagen.fit(X_train)
-    model_net_name = 'model_net_{}_{}_isImg_{}.h5'.format(rows_or_cols, tiles_per_dim, is_image)
-    resnet_rows_cols.save(model_net_name)
+    # model_net_name = 'w.h5'.format(rows_or_cols, tiles_per_dim, is_image)
+    # resnet_rows_cols.save(model_net_name)
     # resnet_rows_cols = keras.models.load_model(model_net_name)
-    # resnet_rows_cols.load_weights('model_weights_{}_{}_isImg_{}.h5'.format(rows_or_cols, tiles_per_dim, is_image), by_name=True)
+    model.load_weights('w_{}_{}_{}.h5'.format(rows_or_cols, tiles_per_dim, is_image))
     # print("loaded")
-    ckpt = keras.callbacks.ModelCheckpoint('model_weights_{}_{}_isImg_{}.h5'.format(rows_or_cols, tiles_per_dim, is_image), monitor='val_acc',
-                                    verbose=1, save_best_only=True, save_weights_only=True, mode='max', period=1)
+    # ckpt = keras.callbacks.ModelCheckpoint('model_weights_{}_{}_isImg_{}.h5'.format(rows_or_cols, tiles_per_dim, is_image), monitor='val_acc',
+    #                                 verbose=1, save_best_only=True, save_weights_only=True, mode='max', period=1)
     early_stop = keras.callbacks.EarlyStopping('val_acc',min_delta=0.001,patience=120)
 
     # for e in range(maxepoches):
-    resnet_rows_cols_hist = resnet_rows_cols.fit_generator(datagen_img_vs_doc_train, validation_data=datagen_img_vs_doc_val,
-                                                           steps_per_epoch=steps_per_epoch, validation_steps=3, epochs=maxepoches,
-                                                           callbacks=[early_stop, ckpt])
+    resnet_rows_cols_hist = model.fit_generator(datagen_img_vs_doc_train, validation_data=datagen_img_vs_doc_val,
+                                                           steps_per_epoch=steps_per_epoch, validation_steps=3, epochs=maxepoches)
+    model.save_weights('w'.format(rows_or_cols, tiles_per_dim, is_image))
 
-    resnet_rows_cols.save_weights('model_weights_{}_{}_isImg_{}.h5'.format(rows_or_cols, tiles_per_dim, is_image))
+    # files = os.listdir('example_docs/')
+    # files.sort()
+    # print(files)  # TODO: remove
+    # images = []
+    # for f in files:
+    #     if 'DS' in f:
+    #         continue
+    #     im = cv2.imread('example_docs/' + f)
+    #     im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+    #     im = preprocess_image(im, c)
+    #     images.append(im)
+    # res = model.predict_on_batch(np.array(images))#, steps=10)
+    # print(np.argmax(res,1))
+
+
 
 
--- project/runner_cols_rows.py
+++ project/runner_cols_rows.py
@@ -4,13 +4,13 @@ from resnet_rows_cols_folder_based_lessKeras import *
 for i in range(1):  # for majority vote
     # mID = str(i) + "_" + str(time.time())
     for is_images in [False]:
-        for tiles_per_dim in [4,5,2]:  # [4]: #
+        for tiles_per_dim in [4]:#,5,2]:  # [4]: #
             # for rows_or_cols in :
             # if tiles_per_dim == 5:
             #     rows_or_cols_list = ["rows"]
             # else:
             #     rows_or_cols_list = ["cols"]
-            for rows_or_cols in ["rows", "cols"]:
+            for rows_or_cols in ["rows"]:#, "cols"]:
                 c = Conf(tiles_per_dim=tiles_per_dim, max_size=112, is_images=is_images)
                 print(c.n_tiles_per_sample)
                 OUTPUT_DIR = "dataset_rows_cols_{}_isImg_{}/".format(tiles_per_dim, is_images)
