accb3c6033c7fb83abddfd26ddc431dfaebec24b
alonalj <alonajurgenson@gmail.com>
2019-02-20 09:31:45 +0200

    ta resnet with weights h5

--- project2.0/resnet_rows_cols_classifier.py
+++ project2.0/resnet_rows_cols_classifier.py
@@ -9,7 +9,18 @@ from keras.models import Model
 import numpy as np
 import os
 from conf import Conf
+import cv2
+import keras
+from keras.layers import Dense, MaxPooling2D, AveragePooling2D, GlobalAveragePooling2D, Conv2D, Dropout, Concatenate, Input
+from keras.layers import Flatten, InputLayer, BatchNormalization, Activation
+from keras.models import Model
+from keras import regularizers, optimizers
+from keras.regularizers import Regularizer
+from keras import backend as K
+import numpy as np
+from keras_preprocessing.image import ImageDataGenerator
 
+from conf import Conf
 
 def get_steps(c, batch_size, data_type):
     relevant_files = get_relevant_files(data_type,c)
@@ -26,7 +37,7 @@ def data_generator(data_type, batch_size, c, rows_or_cols):
         for i in range(get_steps(c, batch_size, data_type)):
             X_batch = []
             y_batch = []
-            for f in relevant_files[batch_size*i:batch_size*(i+1)]:
+            for f in relevant_files[batch_size*i:batch_size*(i+1)]:  # TODO: make balanced precisely?
                 if 'DS' in f:
                     continue
                 # print(f)
@@ -62,34 +73,110 @@ def _evaluate(file_dir='example/'):
     return Y
 
 
-def build_model(c, weights=False):
-    from keras.applications.resnet50 import ResNet50
-    resnet_rows_cols = ResNet50(
-        include_top=False, weights=None, input_tensor=None, input_shape=(c.max_size, c.max_size, 1),
-        pooling=None, classes=c.tiles_per_dim)  # TODO check
-    # Add final layers
-    x = resnet_rows_cols.output
-    x = Flatten()(x)
-    predictions = Dense(c.tiles_per_dim, activation='softmax', name='fc1000',kernel_regularizer=keras.regularizers.l2(0.0001))(x)
+def res_2_layer_block_img_vs_doc(x_in, dim, downsample=False, weight_decay=0.0001):
+    x = Conv2D(dim, kernel_size=(3, 3), padding='same', strides=(2, 2) if downsample else (1, 1),
+               kernel_regularizer=regularizers.l2(weight_decay))(x_in)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
-    # This is the model we will train
-    model = Model(inputs=resnet_rows_cols.input, outputs=predictions)
+    x = Conv2D(dim, kernel_size=(3, 3), padding='same', strides=(1, 1),
+               kernel_regularizer=regularizers.l2(weight_decay))(x)
+    x = BatchNormalization()(x)
 
-    # sgd = optimizers.SGD(lr=0.0001, momentum=0.9, nesterov=True, decay=0.00000001)
-    adam = optimizers.adam(lr=0.0001)
+    if downsample:
+        x_in = Conv2D(dim, kernel_size=(1, 1), padding='same', strides=(2, 2),
+                      kernel_regularizer=regularizers.l2(weight_decay))(x_in)
+
+    x = keras.layers.add([x, x_in])
+    x = Activation('relu')(x)
+
+    return x
+
+
+def res_tower_2_layer_img_vs_doc(x, dim, num_layers, downsample_first=True, weight_decay=0.0001):
+    for i in range(num_layers):
+        x = res_2_layer_block_img_vs_doc(x, dim, downsample=(i == 0 and downsample_first), weight_decay=weight_decay)
+    return x
+
+
+def res_3_layer_block_img_vs_doc(x_in, dim_reduce, dim_out, downsample=False, adjust_skip_dim=False, weight_decay=0.0001):
+    x = Conv2D(dim_reduce, kernel_size=(1, 1), padding='same', strides=(2, 2) if downsample else (1, 1),
+               kernel_regularizer=regularizers.l2(weight_decay))(x_in)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(dim_reduce, kernel_size=(3, 3), padding='same', strides=(1, 1),
+               kernel_regularizer=regularizers.l2(weight_decay))(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(dim_out, kernel_size=(1, 1), padding='same', strides=(1, 1),
+               kernel_regularizer=regularizers.l2(weight_decay))(x)
+    x = BatchNormalization()(x)
+
+    if downsample or adjust_skip_dim:
+        x_in = Conv2D(dim_out, kernel_size=(1, 1), padding='same', strides=(2, 2) if downsample else (1, 1),
+                      kernel_regularizer=regularizers.l2(weight_decay))(x_in)
+
+    x = keras.layers.add([x, x_in])
+    x = Activation('relu')(x)
+
+    return x
+
+
+def res_tower_img_vs_doc(x, dim, num_layers, downsample_first=True, adjust_first=False, weight_decay=0.0001):
+    for i in range(num_layers):
+        x = res_3_layer_block_img_vs_doc(x, int(dim / 4), dim, downsample=(i == 0 and downsample_first),
+                                         adjust_skip_dim=(i == 0 and adjust_first), weight_decay=weight_decay)
+    return x
+
+def build_model(c, weights=False, weight_decay=1e-3):
+    x_in = Input(shape=(c.max_size, c.max_size, 1))
+    x = Conv2D(64, kernel_size=(3, 3), padding='same', strides=(1, 1),
+               kernel_regularizer=regularizers.l2(weight_decay))(x_in)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = res_tower_2_layer_img_vs_doc(x, 64, 2, False, weight_decay=weight_decay)
+    x = res_tower_2_layer_img_vs_doc(x, 128, 2, True, weight_decay=weight_decay)
+    x = res_tower_2_layer_img_vs_doc(x, 256, 2, True, weight_decay=weight_decay)
+    x = res_tower_2_layer_img_vs_doc(x, 512, 2, True, weight_decay=weight_decay)
+
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(c.tiles_per_dim, activation='softmax')(x)
+    model = Model(inputs=x_in, outputs=x)
+    if weights:
+        model.load_weights(weights)
+        print("loaded weights {}".format(weights))
     model.compile(
         loss='categorical_crossentropy',
-        optimizer=adam,
+        optimizer='adam',
         metrics=['accuracy']
     )
-    if weights:
-        d = load_obj(weights)
-        for l_ix in range(len(model.layers)):
-            model.layers[l_ix].set_weights(d[l_ix])
-        print("loaded weights", weights)
-
-    model.summary()
     return model
+
+
+def lr_scheduler(epoch):
+    """Learning Rate Schedule
+    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
+    Called automatically every epoch as part of callbacks during training.
+    # Arguments
+        epoch (int): The number of epochs
+    # Returns
+        lr (float32): learning rate
+    """
+    lr = 0.1
+    if epoch > 100:
+        lr = 0.01
+    if epoch > 150:
+        lr = 0.001
+    if epoch > 200:
+        lr = 1e-4
+    return lr
+
+# def to_grayscale(im):
+#     return cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+
 
 
 def _predict(images):
@@ -116,7 +203,7 @@ def run(c, rows_or_cols):
     datagen_img_vs_doc_train = data_generator('train', batch_size, c, rows_or_cols)
     datagen_img_vs_doc_val = data_generator('val', batch_size, c, rows_or_cols)
 
-    model = build_model(c, weights='weights_no_sim_img_True_t_4_cols_L0.08_A0.97_val_L3.0_A0.33')
+    model = build_model(c, weights='weights_no_sim_img_True_t_4_cols_L7.45_A0.25_val_L4.19_A1.0')
     # model = build_model(c)
     weights_name_format = 'weights_no_sim_img_{}_t_{}_{}'.format(c.is_images, c.tiles_per_dim, rows_or_cols)
     train = True
@@ -155,10 +242,12 @@ def run(c, rows_or_cols):
                 print("Saving model, loss change: {} --> {}, acc change: {} --> {}"
                       .format(round(baseline_loss,2), round(avg_loss_val,2), round(baseline_acc,2), round(avg_acc_val,2)))
                 all_weights = []
-                for l in model.layers:
-                    all_weights.append(l.get_weights())
-                save_obj(all_weights, weights_name_format+'_L{}_A{}_val_L{}_A{}'
+                model.save_weights(weights_name_format+'_L{}_A{}_val_L{}_A{}'
                          .format(round(avg_loss,2), round(avg_acc,2),round(avg_loss_val,2), round(avg_acc_val,2)))
+                # for l in model.layers:
+                #     all_weights.append(l.get_weights())
+                # save_obj(all_weights, weights_name_format+'_L{}_A{}_val_L{}_A{}'
+                #          .format(round(avg_loss,2), round(avg_acc,2),round(avg_loss_val,2), round(avg_acc_val,2)))
                 baseline_loss, baseline_acc = avg_loss_val, avg_acc_val
                 count_plateau = 0
                 if avg_acc_val > 0.9:
