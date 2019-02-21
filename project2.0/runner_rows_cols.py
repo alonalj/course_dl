85becae7631664ff1c046f0a3ce7fea21c999135
alonalj <alonajurgenson@gmail.com>
2019-02-21 17:37:56 +0200

    completing images

--- project2.0/runner_rows_cols.py
+++ project2.0/runner_rows_cols.py
@@ -2,19 +2,10 @@ from conf import Conf
 from preprocessor import split_train_val_test, create_normalization_stats, shred_with_similarity_channel
 from resnet_rows_cols_classifier import run
 
-for i in range(1):  # for majority vote
-    # mID = str(i) + "_" + str(time.time())
-    for is_images in [False]:
-        for tiles_per_dim in [2,4,5]:#,5,2]:  # [4]: #
-
-            # for rows_or_cols in :
-            # if tiles_per_dim == 5:
-            #     rows_or_cols_list = ["rows"]
-            # else:
-            #     rows_or_cols_list = ["cols"]
-            for rows_or_cols in ["cols","rows"]:#, "cols"]:
-                # if is_images and tiles_per_dim == 4 and rows_or_cols == "cols":  # TODO 4, cols
-                #     continue
+for i in range(1):
+    for is_images in [True]:
+        for rows_or_cols in ["rows","cols"]:
+            for tiles_per_dim in [5, 4]:
                 print("Training for: is_images", is_images, tiles_per_dim, rows_or_cols)
                 c = Conf(tiles_per_dim=tiles_per_dim, max_size=112, is_images=is_images)
                 OUTPUT_DIR = "dataset_rows_cols_{}_isImg_{}/".format(tiles_per_dim, is_images)
